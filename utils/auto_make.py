e743dd19028a5fa3ce8e859f7f4d0d926e6e9889
Martijn Bentum <bentummartijn@gmail.com>
2021-03-09 12:51:09 +0100

    typo fix

--- utils/auto_make.py
+++ utils/auto_make.py
@@ -1,13 +1,15 @@
 from texts.models import Text, Language, TextType, Source
 
-languages = 'Dutch,Frysian'.split(',')
-abbreviations= 'nl,fr'.split(',')
+languages = 'Dutch,Frisian,Frisian_dutchized,Dutch_frisianized,English'.split(',')
+abbreviations= 'nl,fr,frl-nl,nl-frl,en'.split(',')
 
 def make_languages(languages=languages,abbreviations=abbreviations, remove_old = False):
 	if remove_old: Language.objects.all().delete()
 	for l, a in zip(languages, abbreviations):
 		language = Language(name=l,abbreviation=a)
-		try:language.save()
+		try:
+			language.save()
+			print('saved:',l)
 		except:print('could not save:',l)
 
 texttypes = 'manual transcription,automatic transcription,council notes'
@@ -21,8 +23,8 @@ def make_texttypes(texttypes=texttypes, remove_old = False):
 		except:print('could not save:',tt)
 
 
-sources = 'frysian radio broadcasts,frysian council notes,frysian council transcripts'
-sources += 'wikipedia,gutenberg,magazine,newspaper,book,cgn,cow'
+sources = 'frisian radio broadcasts,frisian council notes,frisian council transcripts'
+sources += ',wikipedia,gutenberg,magazine,newspaper,book,cgn,cow'
 sources = sources.split(',')
 
 def make_sources(sources = sources,remove_old=False):
