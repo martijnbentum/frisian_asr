a9a4d49c801ea4fb50e99ebf6a466b4893efa0d5
Martijn Bentum <bentummartijn@gmail.com>
2021-03-11 09:44:24 +0100

    typo fix; added title & file id; fixed repr

--- texts/models.py
+++ texts/models.py
@@ -45,10 +45,13 @@ class Text(models.Model):
 	start_time = models.FloatField(default = None,null=True)
 	end_time = models.FloatField(default = None,null=True)
 	wav_filename = models.CharField(max_length=1000,default='')
+	title = models.CharField(max_length=300,default='')
+	file_id= models.CharField(max_length=100,default='')
 
 	def __repr__(self):
 		# f = self.filename.split('/')[-1] if self.filename else ''
-		s = self.source.name.ljust(30)
+		try:s = self.source.name.ljust(30)
+		except:s =''.ljust(30)
 		t = self.raw_text[:70] + '...' if len(self.raw_text) > 70 else self.raw_text
 		return  s + ' | ' + t
 
@@ -58,7 +61,7 @@ class Text(models.Model):
 	@property
 	def transcription(self):
 		from utils.council_transcriptions  import Transcription
-		t = 'frysian council transcripts'
+		t = 'frisian council transcripts'
 		if not self.source.name == t: return 'not available, source is not:'+t
 		if not hasattr(self,'_transcription'):
 			self._transcription = Transcription(self.transcription_meta)
