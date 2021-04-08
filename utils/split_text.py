9471a93ff055f28edbb54206af7fd4fafd608a10
Martijn Bentum <bentummartijn@gmail.com>
2021-04-08 13:56:29 +0200

    simple function to split a text in training dev and test set

---
+++ utils/split_text.py
@@ -0,0 +1,51 @@
+import random
+def split_text(text='',input_filename= '',
+	output_filename='',train = True,dev = True, ratio = .1):
+	'''split a cleaned text into sections train, dev, test
+	text 				a text to be split into training dev test set
+	input_filename 		filename of a text to be split in training dev test set
+	output_filename 	base name which will be postpended with train dev test
+	train 				whether to create a training set, if not text will be
+						split evenly between dev and test
+	dev 				whether to create a development set
+	ratio 				percentage of material reserved for dev and test
+						if both dev and test are created training precentage =
+						1 - ratio *2
+	'''
+	if not text and not input_filename: print('please provide text or input_filename')
+	if not text and input_filename: text = open(input_filename).read()
+	sentences = text.split('\n')
+	n = sum([train,dev])
+	names = []
+	if train:
+		train_ratio = 1 - n *ratio 
+		ratios =[train_ratio] + [ratio] * (n)
+		names.append('train')
+	else: ratios = [0.5,0.5]
+	if dev: names.append('dev')
+	names.append('test')
+	indices = list(range(len(sentences)))
+	random.shuffle(indices)
+	start,end = 0,0
+	output_sentences,output_indices = [],[]
+	for i,r in enumerate(ratios):
+		end = start + int(r * len(sentences))
+		selected_indices = indices[start:end]
+		selected_sentences = extract_from_list(sentences,selected_indices)
+		with open(output_filename + '_' + names[i],'w') as fout:
+			fout.write('\n'.join(selected_sentences))
+		output_sentences.append('\n'.join(selected_sentences))
+		output_indices.append('\n'.join(map(str,selected_indices)))
+		start = end
+	return output_sentences, output_indices
+	
+		
+
+def extract_from_list(l,indices):
+	'''extracts items from list based on a list of indices'''
+	return [l[i] for i in indices]
+
+
+def create_dev_test_council_transcriptions():
+	return split_text(input_filename = '../LM/labels_with_tags.txt',
+		output_filename='../LM/council_transcriptions',train=False)
