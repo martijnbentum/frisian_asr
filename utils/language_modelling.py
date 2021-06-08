b3e05a411a696a12c4901e03ba35131780824f10
Martijn Bentum <bentummartijn@gmail.com>
2021-06-08 17:31:59 +0200

    minor changes for full set training of ngram lm

--- utils/language_modelling.py
+++ utils/language_modelling.py
@@ -12,7 +12,7 @@ lm_dir = '/vol/tensusers/mbentum/FRISIAN_ASR/LM/'
 lm_fame = '/home/eyilmaz/main2/latest_ASR_exps/fame/input/LM_interp_NL_xxxlarge'
 lm_council = lm_dir + 'council.lm'
 
-c0 = 'ngram -order 3 -unk -ppl ../LM/manual_transcriptions_dev -lm '
+c0 = 'ngram -order 3 -unk -ppl ../LM/manual_transcriptions_dev.txt -lm '
 c1 = ' -mix-lm '
 c2 = ' -lambda '
 c3 = ' -debug 2 > '
@@ -36,7 +36,7 @@ def test_fame_council(lamb = .7, debug = 1,output_filename = 'default', run = Fa
 def lambda_test_fame_council():
 	'''assumes lamachine is loaded or SRILM is in path'''
 	#l is lambda, the weight of the main model
-	for l in range(50,105,5):
+	for l in range(5,50,5):
 		command = c0 + lm_fame + c1 + lm_council + c2 + str(l/100) + c3 
 		command += lm_dir + 'fame_council_mix_' + str(l)
 		print(command)
