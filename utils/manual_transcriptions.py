277161ad6986be2fa463383ebf63a9336b021550
Martijn Bentum <bentummartijn@gmail.com>
2021-06-08 17:33:41 +0200

    added Meeting and Line class to analyse the structure of the council recordings to create a fair split for training dev test sets

--- utils/manual_transcriptions.py
+++ utils/manual_transcriptions.py
@@ -2,10 +2,12 @@ import glob
 import os
 import progressbar as pb
 import re
+import random
 import sys
 import textract
 from texts.models import Text, Language, TextType, Source
 from utils import make_tables as mt
+from utils.prefix_meeting_names import prefix_meeting_names
 import progressbar as pb
 
 
@@ -22,28 +24,51 @@ import progressbar as pb
 
 output_dir = '/vol/tensusers/mbentum/FRISIAN_ASR/'
 
-def get_all_council_transcriptions_with_tags(error = False,save = False, with_original=False):
+
+def get_segments(name = 'council',partition = 'train'):
+	if name == 'council': name = 'frisian council transcripts'
+	elif name == 'fame': name = 'frisian radio broadcasts'
+	else:
+		raise ValueError('name should be council or fame')
+	if partition not in 'train,dev,test'.split(','): raise ValueError('partition should be train/dev/test')
+	t = Text.objects.filter(source__name = name)
+	return t.filter(partition = partition)
+	
+
+def make_all_council_transcriptions_with_tags(error = False,save = False, with_original=False,
+	kaldi= True, lm=False):
 	'''
 	save 		whether to save the file
 	with_orig.. whether to add the orginal label (for debugging) 
+	kaldi 		whether to create a line for kaldi AM training
+	lm 			wether to create a line for srilm LM training
 	'''
-	tt = Text.objects.filter(source__name = 'frisian council transcripts').filter(error = error)
-	o = []
-	for t in tt:
-		#provides a line with wav filename start time end time label
-		line = t.transcription.line_with_tags
-		if line:o.append(line) #only append line if there is something in the label
-		if with_original:
-			o.append(t.transcription.line)
-			o.append('-'*9)
-	if save: 
-		filename = output_dir + 'labels_with_tags'
-		if error: filename += '_error'
-		if with_original:filename += '_with_original'
-		filename += '.txt'
-		with open(filename,'w') as fout:
-			fout.write('\n'.join(o))
-	return o
+	if lm: kaldi = False
+	if kaldi: name = 'labels_with_tags_'
+	elif lm: name = 'manual_transcriptions_'
+	print('creating partitions [train/dev/text] and storing as:',name)
+	train_dev_test = []
+	for partition in 'train,dev,test'.split(','):
+		segments = get_segments('council',partition)
+		o = []
+		for segment in segments:
+			#provides a line with wav filename start time end time label
+			if kaldi:line = segment.transcription.line_with_tags
+			if lm:line = segment.transcription.text_with_tags
+			if line:o.append(line)#only append line if there is something in the label
+			if with_original:
+				o.append(t.transcription.line)
+				o.append('-'*9)
+		if save:
+			filename = output_dir + name + partition
+			if error: filename += '_error'
+			if with_original:filename += '_with_original'
+			filename += '.txt'
+			with open(filename,'w') as fout:
+				fout.write('\n'.join(o))
+		train_dev_test.append(o)	
+		print(partition,len(o),'nsegments')
+	return train_dev_test
 
 
 nsn = 'nsn,geluid,oversturing,ruis,klok,hamer,foto,applaus,microfoon,klop'
@@ -54,7 +79,7 @@ spn = ',lipsmack,he,hè,eh,nies,hoest'
 spn = spn.split(',')
 
 
-filename_labels = '/vol/tensusers3/Frisiansubtitling/Downloads-Humainr/second_batch/labels.txt'
+filename_labels = '/vol/tensusers/mbentum/FRISIAN_ASR/labels.txt'
 filename_protocol= '/vol/tensusers3/Frisiansubtitling/Downloads-Humainr/First_Batch_20210218/Transcriptions/protocol_en_tags.txt'
 
 unknown_language= Language.objects.get(name='unknown')
@@ -124,7 +149,7 @@ def add_transcription(t, save = False, source = council_source, check_db = True)
 		o =Text(filetype = 'txt',raw_text = t.text, transcription_meta = t.line, 
 			main_language = t.language, source = source, text_type= text_type,
 			start_time = t.start, end_time = t.end, wav_filename = t.wav,
-			multiple_languages = multiple_languages, error = error)
+			multiple_languages = multiple_languages, error = error, file_id = t.file_id)
 		if not save: return o
 		try: o.save()
 		except:
@@ -141,7 +166,7 @@ class Transcription:
 		self.line = line
 		self.line_type = line_type
 		if line_type == 'council':
-			self.wav, self.l, self.start, self.end, self.text= line.split('\t')
+			self.file_id, self.wav, self.l, self.start, self.end, self.text= line.split('\t')
 		if line_type == 'radio':
 			self.wav = line.filename + '.wav'
 			self.l = line.language
@@ -389,6 +414,9 @@ def get_brackets(line):
 	return brackets,line_without,words,error
 
 class Audio:
+	'''code to analyse the segment audio files, contains multiple segements
+	and is a part of a meeting.
+	'''
 	def __init__(self, audio_filename, texts):
 		self.audio_filename = audio_filename
 		self.texts = texts
@@ -402,6 +430,7 @@ class Audio:
 		return m
 
 def analyse_audio_recordings(wav_dict = None):
+	'''analyse duration of segment audio files.'''
 	if not wav_dict:
 		wav_dict = {}
 		t = Text.objects.filter(source__name='frisian council transcripts')
@@ -412,3 +441,272 @@ def analyse_audio_recordings(wav_dict = None):
 	for key in wav_dict:
 		audios.append(Audio(key,wav_dict[key]))
 	return audios
+
+
+
+def make_meetings():
+	labels = [line.split('\t') for line in open(filename_labels).read().split('\n')]
+	o = {}
+	bads = []
+	for x in labels:
+		count = 0
+		prefs = []
+		found = False
+		for prefix in prefix_meeting_names:
+			if prefix not in o.keys():
+				o[prefix] = []
+			if prefix in x[0]:
+				o[prefix].append(x)
+				count +=1
+				prefs.append(prefix)
+				found = True
+		if not found:bads.append(x)
+		if count >1:print(prefs,'\n',x,count)
+	meetings = {}
+	for prefix in o.keys():
+		meetings[prefix] = Meeting(prefix,o[prefix])
+	return meetings,o, bads
+
+class Meeting:
+	'''represents a specific meeting based on a specific prefix (part of the meeting_wav.
+	the meetings contains consecutive segments (i.e. seg_wav), a seg_wav contains
+	multiple segements.
+	A specific segment is stored in a Line.
+	''' 
+	def __init__(self,prefix,lines):
+		self.prefix = prefix
+		self._org_lines = lines
+		self._set_info()
+		
+	def _set_info(self):
+		duration = 0
+		self.lines = []
+		for i,line in enumerate(self._org_lines):
+			self.lines.append(Line(line,i+1,duration, self))
+			duration += self.lines[-1].duration
+
+	def __repr__(self):
+		m = str(make_time(self.duration)) + ' | ' + str(self.nsegments).ljust(4) 
+		m += ' | ' + str(self.nlabels)
+		return m
+
+	@property
+	def duration(self):
+		return sum([x.duration for x in self.lines])
+
+	@property
+	def nsegments(self):
+		return len(list(set([x.seg_wav for x in self.lines])))
+
+	@property
+	def nlabels(self):
+		return len(self.lines)
+
+class Line:
+	'''represents a segment in a Meeting.'''
+	def __init__(self,line,index,start_in_meeting = None,meeting = None):
+		if type(line) == str: line = line.split('\t')
+		self.line = '\t'.join(line)
+		self.meeting_wav,self.seg_wav,self.language,self.start,self.end,self.text = line
+		self.start = float(self.start)
+		self.end= float(self.end)
+		self.duration = self.end - self.start
+		self.start_in_meeting = start_in_meeting
+		self.index = index
+		self.meeting = meeting
+
+	def __repr__(self):
+		m = self.meeting_wav + ' | ' + make_time(self.duration) 
+		m += ' | ' + make_time(self.start_in_meeting)
+		m += ' | ' + str(self.index)
+		return m
+
+	@property
+	def transcription(self):
+		if not hasattr(self,'_transcription'): self._transcription = Transcription(self.line)
+		return self._transcription
+
+	@property
+	def id_line(self):
+		l = self.transcription.line_with_tags
+		o = [self.meeting.prefix,str(self.index),self.meeting_wav,self.seg_wav]
+		o += [str(self.start),str(self.end),self.text,make_time(self.start_in_meeting)]
+		if not l:
+			o += [str(self.start_in_meeting),self.line]
+		else:
+			o += [str(self.start_in_meeting),self.transcription.line_with_tags]
+		return '\t'.join(o)
+
+	def equal_to_text(self,text):
+		if self.text == text.raw_text and self.start == text.start_time and self.end == text.end_time:
+			return self.meeting_wav == text.file_id and self.seg_wav == text.wav_filename
+		return False
+		
+
+def make_time(seconds):
+	'''helper function to turn seconds in to hours:minutes:seconds'''
+	seconds = int(seconds)
+	h = str(seconds //3600)
+	seconds = seconds % 3600
+	m = str(seconds // 60)
+	s = str(seconds % 60)
+	if len(h) == 1:h =  '0' + h
+	if len(m) == 1:m =  '0' + m
+	if len(s) == 1:s = '0' + s
+	return ':'.join([h,m,s])
+			
+
+def make_train_dev_test(meetings,perc = 0.1, seed = 1111,meeting_test_duration = 1800,
+	save = False):
+	'''partitions the meetings in a training, dev and test set. 
+	perc 					the amount of materials in dev and in test partition
+	seed 					number to freeze the randomization
+	meeting_test_duration 	length from specific meetings reserved for dev or test
+	save 					whether to save the files
+	returns segments for training, dev, test
+	segments from dev and test are consecutive segments (upto meeting_test_duration length)
+	meetings used for dev are not used for test
+	all remaining segments are put into training
+	'''
+	print(perc,seed,meeting_test_duration,save)
+	random.seed(seed)
+	total_duration = sum([meetings[k].duration for k in meetings.keys()])
+	dev_test_duration = total_duration * perc
+	n_test_meetings = int(round(dev_test_duration / meeting_test_duration,0))
+	print('total duration:',make_time(total_duration))
+	print('dev test duration:',make_time(dev_test_duration))
+	print('n dev test meetings:',n_test_meetings)
+	dev_test_meetings = random.sample(meetings.keys(),n_test_meetings *2)
+	dev_meetings = dev_test_meetings[:n_test_meetings]
+	test_meetings = dev_test_meetings[n_test_meetings:]
+	print('\ndev meetings:\n','\n'.join(dev_meetings))
+	print('\ntest meetings:\n','\n'.join(test_meetings))
+	train,dev, test = [],[],[]
+	for k in meetings.keys():
+		if k not in dev_test_meetings:train.extend(meetings[k].lines)
+		elif k in dev_meetings:
+			temp_dev,other = _extract_segments_from_meeting(meetings[k],meeting_test_duration)
+			dev.extend(temp_dev)
+			train.extend(other)
+		elif k in test_meetings:
+			temp_test,other = _extract_segments_from_meeting(meetings[k],meeting_test_duration)
+			test.extend(temp_test)
+			train.extend(other)
+	check_train_dev_test(train,dev,test)
+	if save:
+		_save_set(train,'train')
+		_save_set(dev,'dev')
+		_save_set(test,'test')
+	return train,dev,test
+		
+
+
+
+def _extract_segments_from_meeting(meeting,meeting_test_duration):
+	'''extracts consecutive segments from a single meeting. The start time is random, but
+	starts no later that meeting duration minus meeting_test_duration. 
+	return the selected segments (within random start + meeting_test_duration) as output
+	all other segments are returned in other
+	'''
+	duration = meeting.duration
+	if duration < meeting_test_duration: return meeting.lines
+	start_time = random.randint(0,int(duration - meeting_test_duration))
+	output = []
+	other = []
+	total_line_duration = 0
+	for line in meeting.lines:
+		if start_time > line.start_in_meeting + line.duration: other.append(line)
+		elif total_line_duration > meeting_test_duration:other.append(line)
+		else:
+			output.append(line)
+			total_line_duration += line.duration
+	print('\nmeeting:',meeting.prefix,'\n')
+	print('duration meeting:',meeting.duration, start_time)
+	print('duration selected lines:',sum([l.duration for l in output]))
+	print('goal duration:',meeting_test_duration)
+	print('nlines:',len(output),'\n')
+	return output,other
+
+
+def _save_set(lines,set_type = 'train'):
+	'''saves the train, dev and test partitions.
+	in addition saves an additional info file to identify the segments and a not used
+	file for the segments that were excluded'''
+	output = []
+	output_id = []
+	not_used = []
+	for line in lines:
+		x = line.transcription.line_with_tags
+		if x:
+			output.append(x)
+			output_id.append(line.id_line)
+		else:not_used.append(line.id_line)
+	with open('../council_'+set_type,'w') as fout:
+		fout.write('\n'.join(output))
+	with open('../council_'+set_type+'_id','w') as fout:
+		fout.write('\n'.join(output_id))
+	with open('../council_'+set_type+'_not_used','w') as fout:
+		fout.write('\n'.join(not_used))
+
+def _in_other(source,other):
+	for x in source:
+		if x in other: return True
+	return False
+	
+def check_train_dev_test(train,dev,test):
+	'''checks if there is no overlap in segments between the train, dev and test partitions'''
+	no_overlap =True
+	if _in_other(train,dev):
+		print('lines in train are found in dev')
+		no_overlap = False
+	if _in_other(train,test):
+		print('lines in train are found in test')
+		no_overlap = False
+	if _in_other(test,dev):
+		print('lines in test are found dev')
+		no_overlap = False
+	if no_overlap: print('no overlap between sets')
+
+	print('train segments duration:',make_time(sum([x.duration for x in train])))
+	print('dev segments duration:',make_time(sum([x.duration for x in dev])))
+	print('test segments duration:',make_time(sum([x.duration for x in test])))
+	
+
+def find_partition(texts,train,dev,test, save = False):
+	'''stores the partition a text belongs to in the sqlite database'''
+	train_count,dev_count,test_count = 0,0,0
+	bar = pb.ProgressBar()
+	bar(range(len(texts)))
+	ntrain, ndev, ntest = len(train), len(dev), len(test)
+	for i,text in enumerate(texts):
+		bar.update(i)
+		found = False
+		for j,line in enumerate(test):
+			if line.equal_to_text(text):
+				text.partition = 'test'
+				test_count += 1
+				found = True
+				test.pop(j)
+				break
+		if not found:
+			for j,line in enumerate(dev):
+				if line.equal_to_text(text):
+					text.partition = 'dev'
+					dev_count += 1
+					found = True
+					dev.pop(j)
+					break
+		if not found:
+			for j,line in enumerate(train):
+				if line.equal_to_text(text):
+					text.partition = 'train'
+					train_count += 1
+					found = True
+					train.pop(j)
+					break
+		if save:
+			text.save()
+	print('train:',ntrain,train_count)
+	print('dev:',ndev,dev_count)
+	print('test:',len(test),test_count)
+
