31eefb6fdb9851a84729b2e12ec42bfec3ee2fe6
Martijn Bentum <bentummartijn@gmail.com>
2021-06-29 13:55:57 +0200

    use NN pytorch model to classify the speaker of the humainr data

---
+++ utils/frisian_speaker_classification.py
@@ -0,0 +1,94 @@
+import glob
+from utils import make_kaldi_files as mkf
+from texts.models import Text 
+from . import prefix_meeting_names as pmn
+pmn = pmn.prefix_meeting_names
+from SPEAK_RECOG import predict
+
+
+
+def make_pmn_dict():
+	'''create a dictionary of all frisian council meetings mapping those to the 
+	related transcriptions.
+	'''
+	t = Text.objects.filter(source__name = 'frisian council transcripts')
+	d = {}
+	for name in pmn:
+		d[name] = t.filter(source__name = 'frisian council transcripts',
+			file_id__icontains = name)
+	return d
+
+
+def meeting2speakerids(meeting,p, meeting_name, save = False): 
+	timestamps, speakers, texts= p.predict_meeting(meeting)
+	assert len(speakers) == len(texts)
+	speaker2text, text2speaker = {},{}
+	for i,text in enumerate(texts):
+		speaker_num = str(speakers[i])
+		speaker_num = '0' + speaker_num if len(speaker_num) == 1 else speaker_num
+		speaker_id = meeting_name + '_speaker_'+ speaker_num
+		text2speaker[text.pk] = speaker_id
+		if speaker_id not in speaker2text.keys(): speaker2text[speaker_id] = [] 
+		speaker2text[speaker_id].append(text.pk)
+		text.speaker_id = speaker_id
+		if save:text.save()
+	return text2speaker, speaker2text, texts
+			
+		
+
+
+def classify_speakers(model_dir = "cgn_speaker_model_v3/", device=1, save = False, start_index = None):
+	p = predict.PyTorchPredictor(model_dir + "configs.pth", 
+		model_dir + "weights_best.pth",device=device)
+	pmnd = make_pmn_dict()
+	d = {}
+	for k in pmnd.keys():
+		print(k)
+		if start_index:
+			if list(pmnd.keys()).index(k) < start_index:
+				print('skipping:',k)
+				continue
+		meeting = pmnd[k]
+		text2speaker,speaker2text,texts = meeting2speakerids(meeting,p,k,save)
+		d[k] = [text2speaker, speaker2text, texts]
+	return d
+
+
+def wav2speakerids(wav_filename,p,save=False):
+	timestamps, speakers, texts = p.predict_wav(wav_filename)
+	if not texts: return False, False, False # no text associated with wav file
+	assert len(speakers) == len(texts)
+	speaker2text, text2speaker = {},{}
+	for i,text in enumerate(texts):
+		speaker_num = str(speakers[i])
+		speaker_num = '0' + speaker_num if len(speaker_num) == 1 else speaker_num
+		speaker_id = text.wav_filename.split('.')[0] + '_speaker_'+ speaker_num
+		text2speaker[text.pk] = speaker_id
+		if speaker_id not in speaker2text.keys(): speaker2text[speaker_id] = [] 
+		speaker2text[speaker_id].append(text.pk)
+		text.speaker_id = speaker_id
+		if save:text.save()
+	return text2speaker, speaker2text, texts
+
+
+def classify_speakers_wav(model_dir = "cgn_speaker_model_v3/", device=1, 
+	save = False, start_index = None):
+	p = predict.PyTorchPredictor(model_dir + "configs.pth", 
+		model_dir + "weights_best.pth",device=device)
+	pmnd = make_pmn_dict()
+	d = {}
+	fn = glob.glob(mkf.council_wav_dir +'*.wav')
+	for i,f in enumerate(fn):
+		print(f,i,len(fn))
+		if start_index:
+			if fn.index(f) < start_index:
+				print('skipping:',f)
+				continue
+		text2speaker,speaker2text,texts = wav2speakerids(f,p,save)
+		if not text2speaker: 
+			print('no texts did nothing')
+			continue
+		d[f] = [text2speaker, speaker2text, texts]
+	return d
+	
+
